a5e6a796ff0227e141a47acc1c1b154d7470e818
Abduliman1 <136388103+Abduliman1@users.noreply.github.com>
2023-06-13 18:14:24 +0100

    Update FacebookStatusUpdate.java

--- Facebook2/src/main/java/FirstTest/FacebookStatusUpdate.java
+++ Facebook2/src/main/java/FirstTest/FacebookStatusUpdate.java
@@ -23,36 +23,36 @@ public class FacebookStatusUpdate {
     @Test
     public void testFacebookStatusUpdate() {
        
-    	// Open Facebook login page
+    	// login page
     	driver.get("https://web.facebook.com/"); 
 
-        // Enter email
+        // email
         driver.findElement(By.id("email")).sendKeys("abdulimanautomation@gmail.com");
         
-        // Enter password
+        // password
         driver.findElement(By.id("pass")).sendKeys("Automation1");
         
-        // Click on login button
+        // login button
         driver.findElement(By.name("login")).click();
         
         WebDriverWait wait = new WebDriverWait(driver, 10); // Create WebDriverWait instance
         
-        // Click on the Facebook logo
+        // Facebook logo
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@aria-label='Facebook']"))).click();
         
-        // Click on "Create story"
+        // "Create story"
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@aria-label='Create story']"))).click();
         
-        // Click on the story creation preview
+        // story creation preview
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@aria-label='Stories creation preview']/div/div/div/div/div[2]/div"))).click();
         
-        // Click on the "Start typing" field
+        // "Start typing" field
         wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//label[@aria-label='Start typing']"))).click();
         
-        // Enter the text "Hello World"
+        // text "Hello World"
         driver.findElement(By.xpath("//label[@aria-label='Text']")).sendKeys("Hello World");
         
-        // Click on the "Share to Story" button
+        // "Share to Story" button
         driver.findElement(By.xpath("//div[@aria-label='Share to Story']/div/div/div/span/span")).click();
     }
 
